9f8ed244f83e2b8142676a1faa7e4bfc4c040ada
Stitch <690986754@qq.com>
2021-08-19 11:20:43 +0800

    Initial commit

---
+++ src/main/java/com/example/webfluxdemo/controller/UserController.java
@@ -0,0 +1,37 @@
+package com.example.webfluxdemo.controller;
+
+import com.example.webfluxdemo.entity.User;
+import com.example.webfluxdemo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class UserController {
+
+    //注入service
+    @Autowired
+    private UserService userService;
+
+    //id查询
+    @GetMapping("/user/{id}")
+    public Mono<User> getUserId(@PathVariable int id) {
+        return userService.getUserById(id);
+    }
+
+    //查询所有
+    @GetMapping("/user")
+    public Flux<User> getUsers() {
+        return userService.getAllUser();
+    }
+
+    //添加
+    @PostMapping("/saveuser")
+    public Mono<Void> saveUser(@RequestBody User user) {
+        Mono<User> userMono = Mono.just(user);
+        return userService.saveUserInfo(userMono);
+    }
+}
+
